c6e7591443be29cd5598ed0e4f07ff8ca80628cc
davud600 <davudgorqi@gmail.com>
2023-09-22 18:59:24 +0200

    changes

--- src/index.ts
+++ src/index.ts
@@ -15,56 +15,163 @@ export default class ShopifyOrderManager {
     this.apiUrl = apiUrl
   }
 
-  public async uploadOrderFromJson(ordersJsonFilePath: string) {
-    const rawdata = fs.readFileSync(ordersJsonFilePath)
-    const orders = JSON.parse(rawdata.toString())
+  public async deleteOrdersFromShopifyWithFilter(
+    filterCallback: (order: any) => any[]
+  ) {
+    // Get All order ids
+    const res = await fetch(`${this.apiUrl}/orders.json?status=any&limit=250`, {
+      method: 'get',
 
-    const updatedOrdersArray = orders.map((order: any) => {
+      headers: {
+        'X-Shopify-Access-Token': this.accessToken,
+        'content-type': 'application/json',
+      },
+    })
+    const data = (await res.json()) as {
+      orders: { id: string; cancel_reason?: string }[]
+    }
+    const orders = data.orders.map((order) => {
       return {
-        line_items: [
-          {
-            product_id: order['Product ID'],
-            variant_id: order['Variant ID'],
-            sku: order['SKU'],
-            price: 59.9,
-            quantity: 1,
-          },
-        ],
-        phone: order['Billing Address Phone'],
-        customer: {
-          email: order['Customer Email'],
-          first_name: order['Customer First Name'],
-          last_name: order['Customer Last Name'],
-          note: order['Note'],
-          currency: order['Transaction currency'],
-          addresses: [],
-        },
-        billing_address: {
-          address1: order['Billing Address'],
-          city: order['Billing Address City'],
-          country: order['Billing Address Country'],
-          first_name: order['Billing Address First Name'],
-          last_name: order['Billing Address Last Name'],
-          province: order['Billing province state'],
-          zip: order['Billing Address Zip'],
-          country_code: order['Shipping Address Country'],
-        },
-        shipping_address: {
-          address1: order['Shipping Address'],
-          city: order['Shipping Address City'],
-          country: order['Shipping Address Country'],
-          first_name: order['Shipping Address First Name'],
-          last_name: order['Shipping Address Last Name'],
-          province: order['Shipping Address Province State'],
-          zip: order['Shipping Address Zip'],
-          country_code: order['Shipping Address Country'],
-        },
-        current_total_price: '59.90',
-        currency: 'USD',
-        fulfillment_status: 'fulfilled',
-        tracking_number: order['Tracking Numbers'],
+        id: order.id,
+        canceled: order.cancel_reason != null,
       }
     })
+    const ordersToDelete = orders.filter(filterCallback)
+    console.log(`Orders to delete: ${ordersToDelete.length}`)
+
+    // Delete all orders
+    for (let index = 0; index < ordersToDelete.length; index++) {
+      try {
+        await fetch(`${this.apiUrl}/orders/${ordersToDelete[index].id}.json`, {
+          method: 'delete',
+          headers: {
+            'X-Shopify-Access-Token': this.accessToken,
+            'content-type': 'application/json',
+          },
+        })
+      } catch (error) {
+        console.error(error)
+        break
+      }
+      console.log(`Deleted order with id: ${ordersToDelete[index].id}`)
+    }
+
+    console.log('Deleted all orders')
+  }
+
+  public async deleteOrdersFromShopify() {
+    // Get All order ids
+    const res = await fetch(`${this.apiUrl}/orders.json?status=any&limit=250`, {
+      method: 'get',
+
+      headers: {
+        'X-Shopify-Access-Token': this.accessToken,
+        'content-type': 'application/json',
+      },
+    })
+    const data = (await res.json()) as { orders: { id: string }[] }
+    const orderIds = data.orders.map((order) => order.id)
+
+    // Delete all orders
+    for (let index = 0; index < orderIds.length; index++) {
+      try {
+        await fetch(`${this.apiUrl}/orders/${orderIds[index]}.json`, {
+          method: 'delete',
+
+          headers: {
+            'X-Shopify-Access-Token': this.accessToken,
+            'content-type': 'application/json',
+          },
+        })
+      } catch (error) {
+        console.error(error)
+        break
+      }
+
+      console.log(`Deleted order number: ${index}`)
+    }
+
+    console.log('Deleted all orders')
+  }
+
+  public async getOrdersFromShopify() {
+    const res = await fetch(`${this.apiUrl}/orders.json?status=any&limit=250`, {
+      method: 'get',
+
+      headers: {
+        'X-Shopify-Access-Token': this.accessToken,
+        'content-type': 'application/json',
+      },
+    })
+    const data = await res.json()
+
+    return data
+  }
+
+  public async uploadOrdersFromJson(
+    ordersJsonFilePath: string,
+    ordersMappingCallback: (order: any) => any[]
+  ) {
+    const rawdata = fs.readFileSync(ordersJsonFilePath)
+    const orders = JSON.parse(rawdata.toString())
+    let updatedOrdersArray = []
+
+    if (!!!ordersMappingCallback) {
+      updatedOrdersArray = orders.map((order: any) => {
+        return {
+          line_items: order['Products'].map((product: any) => {
+            return {
+              product_id: product['Product ID'],
+              variant_id: product['Variant ID'],
+              sku: product['SKU'],
+              price: product['Price'],
+              quantity: product['Quantity'],
+            }
+          }),
+          phone: order['Billing Address Phone'],
+          customer: {
+            email: order['Customer Email'],
+            first_name: order['Customer First Name'],
+            last_name: order['Customer Last Name'],
+            note: order['Note'],
+            currency: order['Transaction currency'],
+            addresses: [],
+          },
+          billing_address: {
+            address1: order['Billing Address'],
+            city: order['Billing Address City'],
+            country: order['Billing Address Country'],
+            first_name: order['Billing Address First Name'],
+            last_name: order['Billing Address Last Name'],
+            province: order['Billing province state'],
+            zip: order['Billing Address Zip'],
+            country_code: order['Shipping Address Country'],
+          },
+          shipping_address: {
+            address1: order['Shipping Address'],
+            city: order['Shipping Address City'],
+            country: order['Shipping Address Country'],
+            first_name: order['Shipping Address First Name'],
+            last_name: order['Shipping Address Last Name'],
+            province: order['Shipping Address Province State'],
+            zip: order['Shipping Address Zip'],
+            country_code: order['Shipping Address Country'],
+          },
+          current_total_price: order['Products']
+            .reduce(
+              (accumulator: number, currentValue: { price: number }) =>
+                accumulator + currentValue.price,
+              0
+            )
+            .toString(),
+          currency: 'USD',
+          fulfillment_status: 'fulfilled',
+          tracking_number: order['Tracking Numbers'],
+        }
+      })
+    } else {
+      updatedOrdersArray = orders.map(ordersMappingCallback)
+    }
 
     for (let index = 0; index < updatedOrdersArray.length; index++) {
       const order = updatedOrdersArray[index]
